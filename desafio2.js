8a268df260ec3c1a7852c8e65aba165e1a9ebfdc
LucianoBazaes <lucho.bazaes@gmail.com>
2023-02-02 19:09:51 -0300

    Desafio N°2

--- desafio2.js
+++ desafio2.js
@@ -24,17 +24,13 @@ class ProductManager {
     //Validacion para que los campos esten completos
     if (Object.values(newProduct).includes(undefined || "")) {
       console.log("Los campos requeridos deben estar completos");
-
     } else {
-
       const productos = await this.getProduct();
       //Obtengo un arreglo con todos los codigos de mis productos y luego con el metodo includes verifico si el codigo nuevo producto ya existe o no.
-      
+
       if (productos.map((item) => item.code).includes(newProduct.code)) {
         console.log(`El código ${newProduct.code} ya existe`);
-
-      }  else {
-
+      } else {
         //Aca el ID se incrementa a medida que se agrega un producto nuevo al array "Products"
         ProductManager.id = ProductManager.id + 1;
 
@@ -57,7 +53,7 @@ class ProductManager {
     const productos = await this.getProduct();
 
     let productId = productos.find((p) => p.id == id);
-  
+
     if (productId) {
       return productId;
     } else {
@@ -66,26 +62,30 @@ class ProductManager {
     }
   }
 
-
   //Metodo para eliminar un producto mediante su id
   async deleteProduct(id) {
     const productos = await this.getProduct();
 
-    let filterProdId = productos.filter((p) => p.id != id)
+    let filterProdId = productos.filter((p) => p.id != id);
 
     console.log("Delete Product");
-    
+
     await fs.writeFile(this.path, JSON.stringify(filterProdId), "utf-8");
   }
-  
-  async updateProduct({id,...producto}) {
-    await this.deleteProduct(id);
-    const productos = await this.getProduct();
-    const productosModificados =[{...producto, id}, ...productos];
-    await fs.writeFile(this.path, JSON.stringify(productosModificados), "utf-8");
-}
-}
 
+  async updateProduct(id, atributos) {
+    const productos = await this.getProduct();
+    const prodAModif = productos.find((p) => p.id === id);
+    const updatedProduct = { ...prodAModif, ...atributos };
+    const updatedProducts = productos.map((p) => (p.id) !== id ? p : updatedProduct);
+    await fs.writeFile(this.path, JSON.stringify(updatedProducts), "utf-8");
+
+    // await this.deleteProduct(id);
+    // const productos = await this.getProduct();
+    // const productosModificados =[{...producto, id}, ...productos];
+    // await fs.writeFile(this.path, JSON.stringify(productosModificados), "utf-8");
+  }
+}
 
 //Instancio la clase ProductManager
 const productos = new ProductManager();
@@ -140,18 +140,6 @@ await productos.addProduct(
 //productos.deleteProduct(1)
 
 //Metodo para modificar un producto
-productos.updateProduct(
-  {
-    title: 'Buzo Nike',
-    description: 'Buzo Nike de color negro deportivo',
-    price: 14000,
-    thumbnail: 'image2',
-    code: 'ABC234',
-    stock: 15,
-    id: 2
-  }
-)
+let prodMod = await productos.updateProduct(2,{ stock: 15 });
 
-
-
-
+console.log(prodMod);
